cc0a103673959571bde4033471130b572f15d358
Ricky Lee <rickylee150@gmail.com>
2022-09-19 00:49:50 -0400

    index.jsx view rendered at pokemon route

--- servers.js
+++ servers.js
@@ -4,12 +4,15 @@ const port = 3000
 
 const pokemon = require('./models/pokemon.js')
 
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 app.get('/', function(req,res){
     res.send('Welcome to the Pokemon App!')
 })
 
 app.get('/pokemon', function(req,res){
-    res.send(pokemon)
+    res.render('Index')
 })
 
 app.listen(port, function() {
---
+++ views/index.jsx
@@ -0,0 +1,17 @@
+const React = require('react')
+const myStyle = {
+    color: '#ffffff',
+    backgroundColor: '#000000',
+};
+
+class Index extends React.Component {
+
+    render() {
+        return (
+            <h1 style={myStyle}> See All The Pokemon! </h1>
+        );
+    }
+}
+
+
+module.exports = Index;
